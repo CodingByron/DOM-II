098886fb0794872f43e3214a2a7517af6756858e
Byron Sanders <byrone_sanders@hotmail.com>
2023-06-11 12:06:34 -0700

    Created JS code for load and copy

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-import './less/index.less'
+import "./less/index.less";
 
 // Your code goes here!
+
+//load
+window.onload = function (evt) {
+  console.log(`event ${evt.type} fired! Ready to go!`);
+  const heading = document.querySelector("h1");
+  heading.textContent = "READY TO GO!!";
+
+  //copy
+  window.addEventListener("copy", () => {
+    navigator.clipboard.readText().then((text) => {
+      heading.textContent += text;
+    });
+  });
+};
